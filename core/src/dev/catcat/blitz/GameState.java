6ea41f4fba1b8ed6ca32b6d335a4ed5118b52bfe
tcadamson <taylor@catcat.dev>
2021-02-04 06:47:25 -0700

    Add rudimentary character controller

--- core/src/dev/catcat/blitz/GameState.java
+++ core/src/dev/catcat/blitz/GameState.java
@@ -1,6 +1,8 @@
 package dev.catcat.blitz;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
@@ -28,14 +30,18 @@ public class GameState implements Screen {
     private Map<String, Color> colors;
     private SpriteBatch batch;
     private TextureAtlas atlas;
-    private TextureRegion cell;
+    private TextureRegion quad;
     private Vector2 size;
-    private double accumulator;
+    private Vector2 dir;
+    private Body collider;
+    private float accumulator;
     private final float SCALE = 0.15f;
     private final float MAX = 0.25f;
     private final float DT = 1/60f;
     private final int DX = 6;
     private final int DS = 2;
+    private final int DAMP = 20;
+    private final int THRUST = 2500;
 
     GameState(Launcher game) {
         world = new World(new Vector2(), true);
@@ -45,9 +51,49 @@ public class GameState implements Screen {
         colors = new HashMap<>();
         batch = new SpriteBatch();
         atlas = new TextureAtlas("test.atlas");
-        cell = atlas.findRegion("a");
-        size = new Vector2(cell.getRegionWidth(), cell.getRegionHeight()).scl(SCALE);
-        circle(size.len());
+        quad = atlas.findRegion("a");
+        size = new Vector2(quad.getRegionWidth(), quad.getRegionHeight()).scl(SCALE);
+        dir = new Vector2();
+        collider = circle(size.len());
+        Gdx.input.setInputProcessor(new InputAdapter() {
+            @Override
+            public boolean keyDown(int key) {
+                switch(key) {
+                    case Input.Keys.W:
+                        dir.add(0, 1);
+                        break;
+                    case Input.Keys.S:
+                        dir.add(0, -1);
+                        break;
+                    case Input.Keys.A:
+                        dir.add(-1, 0);
+                        break;
+                    case Input.Keys.D:
+                        dir.add(1, 0);
+                        break;
+                }
+                return true;
+            }
+
+            @Override
+            public boolean keyUp(int key) {
+                switch(key) {
+                    case Input.Keys.W:
+                        dir.add(0, -1);
+                        break;
+                    case Input.Keys.S:
+                        dir.add(0, 1);
+                        break;
+                    case Input.Keys.A:
+                        dir.add(1, 0);
+                        break;
+                    case Input.Keys.D:
+                        dir.add(-1, 0);
+                        break;
+                }
+                return true;
+            }
+        });
         // TODO: import these from some external config file
         colors.put("bg", Color.valueOf("DCE0E0"));
         colors.put("body", Color.valueOf("1D3557"));
@@ -68,9 +114,10 @@ public class GameState implements Screen {
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.setColor(body);
-        batch.draw(cell, camera.position.x - size.x/2, camera.position.y - size.y/2, size.x, size.y);
+        batch.draw(quad, -size.x/2, -size.y/2, size.x, size.y);
         batch.end();
         debug.render(world, camera.combined);
+        collider.applyForceToCenter(dir.cpy().scl(THRUST * collider.getMass()), true);
         // TODO: add interpolation step following accumulator loop
         // see https://saltares.com/games/fixing-your-timestep-in-libgdx-and-box2d/
         accumulator += Math.min(delta, MAX);
@@ -101,14 +148,16 @@ public class GameState implements Screen {
     public void dispose() {
     }
 
-    private void circle(float d) {
+    private Body circle(float d) {
         CircleShape shape = new CircleShape();
         BodyDef def = new BodyDef();
         def.type = BodyDef.BodyType.DynamicBody;
+        def.linearDamping = DAMP;
         Body body = world.createBody(def);
         shape.setRadius(d/2);
         body.createFixture(shape, 1);
         body.setTransform(0, 0, 0);
         shape.dispose();
+        return body;
     }
 }
