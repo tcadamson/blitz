3cda4e807ee186be8735f9cca8d1e90eb544cf03
Taylor Adamson <t27182818@outlook.com>
2021-02-21 18:19:45 -0700

    Upgrade to LWJGL3

--- desktop/src/dev/catcat/blitz/desktop/DesktopLauncher.java
+++ desktop/src/dev/catcat/blitz/desktop/DesktopLauncher.java
@@ -1,15 +1,14 @@
 package dev.catcat.blitz.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import dev.catcat.blitz.Launcher;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "blitz";
-		config.width = 1480;
-		config.height = 720;
-		new LwjglApplication(new Launcher(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setTitle("blitz");
+		config.setWindowedMode(1480, 720);
+		new Lwjgl3Application(new Launcher(), config);
 	}
 }
